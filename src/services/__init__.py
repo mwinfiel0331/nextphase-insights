7c4fbe42a8010071f49269945ae129a9cbabe711
Melissa Bolcar <melissa.bolcar@gmail.com>
2025-04-12 01:09:40 -0400

    Remove Firebase service account credentials

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,1 +1,10 @@
+"""
+Service modules for database and authentication
+"""
+from .db_service import (
+    save_client_data,
+    get_all_clients,
+    save_session_data,
+    get_client_sessions
+)
 
